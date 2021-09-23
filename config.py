c39a63aace269711e884d1b1b1b64e8ef2b1e45a
shalin <shalin.rono@student.moringaschool.com>
2021-09-23 21:49:21 +0300

    create class devConfig

--- config.py
+++ config.py
@@ -24,3 +24,14 @@ class ProdConfig(Config):
     '''
     pass
 
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
