6d1428b360782bab1ff82c569ab6ca3300368461
shalin <shalin.rono@student.moringaschool.com>
2021-09-23 22:15:30 +0300

    initialize app subfolder

---
+++ app/__init__.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    #initializing the application
+    app = Flask(__name__)
+
+    #setting up the configuration
+    app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #Initializing Flask Extenstion
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+
+    #Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+    #setting config
+    from .request import configure_request
+    configure_request(app)
+
+    
+    return app
