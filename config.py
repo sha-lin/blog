363608d011b10d43286061575c1376e429246d40
shalin <shalin.rono@student.moringaschool.com>
2021-09-23 21:43:09 +0300

    create class config in config file

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    # QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://shalin:Chepkoech03@localhost/blogger'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
