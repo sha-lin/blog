fc2c277518f404fd2a46c00466b0404a27810e04
shalin <shalin.rono@student.moringaschool.com>
2021-09-23 22:21:53 +0300

    add quote test

---
+++ tests/quote_test.py
@@ -0,0 +1,15 @@
+import unittest
+from app.models import Quote
+
+
+class QuoteTest(unittest.TestCase):
+    def setUp(self):
+        """Will run before every test"""
+        self.new_quote = Quote(1,"Shalin","It's never that too serious")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_quote,Quote))
+
+    
+if __name__ == '__main__':
+    unittest.main()
