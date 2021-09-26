8fdb6b0a1e7766e2e38e17cf809b0fc280bc9729
shalin <shalin.rono@student.moringaschool.com>
2021-09-26 06:58:36 +0300

    config changes

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://shalin:Chepkoech03@localhost/bloges_test'
